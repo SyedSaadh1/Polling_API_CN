14e2a027bc8abbc79a19af6ce19bcd92c9503240
SyedSaadh1 <saadhsyed1@gmail.com>
2024-02-27 09:49:48 +0530

    commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const Port= process.env.PORT_NUMBER
+const express=require('express')
+const bodyParser=require('body-parser')
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}))
+const db=require('./config/mongoose');
+
+// Routes
+app.use('/',require('./routes/index'));
+   
+app.listen(Port,function(err){
+    if(err){
+        console.log(err);
+    }
+    console.log("server is runing ...",Port);
+})
